a6509db5041a3cefe9971faff63548628558f099
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2019-06-05 23:37:06 -0700

    Create passByRef.go

---
+++ go/passByRef.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	course := "Docker Deep Dive"
+
+	fmt.Println("You are studying the course: ",course)
+
+	changeCourse(&course)
+
+	fmt.Println("Course value after calling changeCourse function: ",course)
+}
+
+	// & references a pointer
+	// * dereferences a pointer
+
+func changeCourse(courseName *string) string{
+	*courseName = "Go Fundamentals"
+	fmt.Println("Inside changeCourse Function, course name is changed as: ",*courseName)
+	return *courseName
+}

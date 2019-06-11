7e136ebf70448d5f118c08eb0006078f35ee8cde
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2019-06-10 22:27:02 -0700

    Create slicer.go

---
+++ go/loops/slicer.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	slicer := []string{"Surya","Vallabhaneni","12345"}
+
+	for i := range slicer {
+		fmt.Println("Length is:",len(slicer))
+		fmt.Println("Elements number is:",i)
+		fmt.Println("Elements number is:",slicer[i])
+	}
+}
